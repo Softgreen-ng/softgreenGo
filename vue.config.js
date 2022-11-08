948dd692b04d29e6ebdee432f3419d656defc29e
bataino <bataino.ronaldo@gmail.com>
2022-11-08 18:04:22 +0100

    Favicon and undefined

--- vue.config.js
+++ vue.config.js
@@ -1,38 +1,40 @@
 module.exports = {
-    configureWebpack: {
-      externals: {
-        Vue: "vue"
-      }
+  configureWebpack: {
+    externals: {
+      Vue: "vue"
+    }
+  },
+  pwa: {
+    manifestOptions: {
+      name: "Softgreen GO",
+      short_name: "Softgreen GO",
+      start_url: "./",
+      display: "standalone",
+      theme_color: "#000000",
+      icons: [
+        {
+          src: "img/icons/favicon-32x32.png",
+          sizes: "512x512",
+          type: "image/svg+xml",
+          purpose: "any maskable",
+        }, , ,
+      ],
     },
-    pwa: {
-      manifestOptions: {
-        name: "App Name",
-        short_name: "Short Name",
-        start_url: "./",
-        display: "standalone",
-        theme_color: "#000000",
-        icons: [
-          {
-            src: "./favicon.svg",
-            sizes: "512x512",
-            type: "image/svg+xml",
-            purpose: "any maskable",
-          },
-        ],
-      },
-  
-      themeColor: "#4DBA87",
-      msTileColor: "#000000",
-      appleMobileWebAppCapable: "yes",
-      appleMobileWebAppStatusBarStyle: "black",
-      iconPaths: {
-        maskicon: null,
-        favicon32: "./favicon32.png",
-        favicon16: "./favicon16.png",
-        appleTouchIcon: null,
-        msTileImage: null,
-      },
-      // configure the workbox plugin
-      workboxPluginMode: "GenerateSW",
+    workboxOptions: {
+      skipWaiting: true
     },
+    themeColor: "#4DBA87",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+    iconPaths: {
+      maskicon: null,
+      favicon32: "img/icons/favicon-32x32.png",
+      favicon16: "img/icons/favicon-16x16.png",
+      appleTouchIcon: null,
+      msTileImage: null,
+    },
+    // configure the workbox plugin
+    workboxPluginMode: "GenerateSW",
+  },
 };
