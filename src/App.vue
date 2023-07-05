03ae6ed64d1af12cb23d4b10b113f081c83a6902
Bataino <bataino.ronaldo@gmail.com>
2023-07-05 05:36:05 +0100

    maintenance mode

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
-  <div class="position-relative header-padding">
-    <support-chat></support-chat>
-    <confirm-dialog></confirm-dialog>
-    <app-header class="w-100 " style="" />
-    <div class="h-100" style="min-height:50vh !important">
-      <router-view v-slot="{ Component }">
-        <transition name="slide">
-          <component :is="Component" />
-        </transition>
-      </router-view>
-    </div>
-    <app-footer class="w-100 mt-5" />
-  </div>
+	<div class="h-100 maintain-bg v-100 d-flex align-items-center justify-content-center p-5 p-md-5"
+		style="min-height: 100svh;" v-if="isMaintenance == true">
+		<app-header class="w-100 " style="" />
+		<div>
+			<img src="https://img.freepik.com/free-vector/maintenance-concept-illustration_114360-3928.jpg"
+				style="max-width: 350px;" />
+			<div>
+				<h1 class="font-1 text-gray fw-bold text-sg-primary">Maintenance Ongoing</h1>
+				<span class="fs-6">
+					We're currently ongoing our website maintentance to make sure we serve you the best
+				</span><br>
+
+				<a class="btn bg-sg-primary mt-5 text-white font-1" href="https://wa.me/234913071412">
+					<div class="fs-6 px-2 shadow-sm">
+						Proceed to Whatsapp
+					</div>
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="position-relative header-padding" v-else>
+		<support-chat></support-chat>
+		<confirm-dialog></confirm-dialog>
+		<app-header class="w-100 " style="" />
+		<div class="h-100" style="min-height:50vh !important">
+			<router-view v-slot="{ Component }">
+				<transition name="slide">
+					<component :is="Component" />
+				</transition>
+			</router-view>
+		</div>
+		<app-footer class="w-100 mt-5" />
+	</div>
 </template>
 
 <script >
@@ -20,60 +40,75 @@ import { getProfile } from './services/user';
 import SupportChat from './components/SupportChat.vue';
 
 export default {
-  name: '',
-  components: {
-    ConfirmDialog,
-    SupportChat
-  },
-  created() {
-    if (this.$store.getters.auth) {
-      getProfile()
-        // .then((response) => {
-          // console.log(response)
-          // if (response.errors) {
-          //   if (response.token_expired) {
-          //     localStorage.removeItem("token")
-          //     this.$toast.error("Session expired", "error")
-          //     this.$router.push("/login")
-          //   }
-          // }
-        // })
-    }
-  }
+	name: '',
+	components: {
+		ConfirmDialog,
+		SupportChat
+	},
+	computed: {
+		isMaintenance() {
+			return true
+		}
+	},
+	created() {
+		if (this.$store.getters.auth) {
+			getProfile()
+			
+			if(this.isMaintenance)
+			setTimeout(() => {
+				this.$toast.success("...Redirecting to whatsapp")
+				setTimeout(() => {
+					location.href = "https://wa.me/234913071412"
+				}, 1000)
+			}, 2000)
+		}
+	}
 }
 </script>
 
 <style lang="less">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
 
 .bg-secondary {
-  --bs-bg-opacity: .5 !important;
+	--bs-bg-opacity: .5 !important;
 }
 
 #nav {
-  padding: 30px;
+	padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+	a {
+		font-weight: bold;
+		color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+		&.router-link-exact-active {
+			color: #42b983;
+		}
+	}
 }
 
 .header-padding {
-  padding-top: 75px;
+	padding-top: 75px;
 
-  @media(min-width:768px) {
-    padding-top: 75px;
-  }
+	@media(min-width:768px) {
+		padding-top: 75px;
+	}
 }
-</style>
+
+.maintain-bg {
+	background-repeat: no-repeat;
+	background-size: cover;
+
+	@media screen and (min-width:768px) {
+		// background-image: url('https://media.istockphoto.com/id/515856750/photo/healthy-eating-background.jpg?s=612x612&w=0&k=20&c=yfOFP_fUFrUFdNM3nb1g0S5FNzbm28-0fSQrqaVar_M=');
+	}
+
+	@media screen and (max-width:768px) {
+		// background-image: url('https://media.istockphoto.com/id/515856750/photo/healthy-eating-background.jpg?s=612x612&w=0&k=20&c=yfOFP_fUFrUFdNM3nb1g0S5FNzbm28-0fSQrqaVar_M=');
+	}
+}</style>
